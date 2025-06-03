ca8587f1b468a3604c180e25c29bdf79347f5823
Gilberto <contato.junior272005@gmail.com>
2025-06-03 20:13:33 -0300

    feat: generated a project modularity report

--- src/main/java/fincontrol/com/fincontrol/exception/ResourceNotFoundException.java
+++ src/main/java/fincontrol/com/fincontrol/exception/ResourceNotFoundException.java
@@ -1,7 +1,8 @@
 package fincontrol.com.fincontrol.exception;
 
 // Não precisa de @ResponseStatus aqui se for tratado pelo @ControllerAdvice
-public class ResourceNotFoundException extends RuntimeException {
+public class
+ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
